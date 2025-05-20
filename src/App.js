14e3863b6b888934a2347f8698c71ae96696908a
Bakhit Mohamad <63530204+bakhiet19@users.noreply.github.com>
2025-05-20 23:25:28 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -88,7 +88,7 @@ export default function App() {
 
 
   useEffect(function(){
-    fetch("http://jsonblob.com/1374497613067509760")
+    fetch("https://jsonblob.com/1374497613067509760")
     .then((res)=> res.json())
     .then((data) => dispatch({type : "dataRecieved" , payload : data}))
     .catch((error) => dispatch({type : "Error"}))
